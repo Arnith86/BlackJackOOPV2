a74ef6bd3d187a052239062bb84a6714720fb6d4
a21jeaha <hisui86@gmail.com>
2025-03-13 15:57:50 +0100

    a product interface "ICard.cs" has been created
    a product interface "ICard.cs" has been created, will be used as part of a factory pattern

---
+++ BlackJackV2/Models/ICard.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackV2.Models
+{
+
+	/* *
+	 * This is the "Product" part of the "Card" factory. 
+	 * Common aspect to all cards is image, generic types are used to ease reuse of this interface
+	 * */
+	internal interface ICard<TImage, TValue>
+	{
+		public TImage Image{ get; set; }
+		public TValue Value { get; set; }
+	}
+}
