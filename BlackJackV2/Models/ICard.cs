90f012006101c2779dcd9a7c0ed1b2ce3dfe7483
a21jeaha <hisui86@gmail.com>
2025-03-13 18:28:23 +0100

    ICard.cs was updated to include be front and back images of the card. also made the properties more restrictive

--- BlackJackV2/Models/ICard.cs
+++ BlackJackV2/Models/ICard.cs
@@ -1,9 +1,6 @@
-﻿using Avalonia.Media.Imaging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using Avalonia.Media.Imaging;
 
 namespace BlackJackV2.Models
 {
@@ -12,9 +9,10 @@ namespace BlackJackV2.Models
 	 * This is the "Product" part of the "Card" factory. 
 	 * Common aspect to all cards is image, generic types are used to ease reuse of this interface
 	 * */
-	internal interface ICard<TImage, TValue>
+	internal interface ICard<TFrontImage, TBackImage, TValue>
 	{
-		public TImage Image{ get; set; }
-		public TValue Value { get; set; }
+		public TFrontImage FrontImage{ get; }
+		public TBackImage BackImage{ get; }
+		public TValue Value { get; }
 	}
 }
