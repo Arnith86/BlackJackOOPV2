2bbcf9932018b3b39c3c1d70cc70560278480276
a21jeaha <hisui86@gmail.com>
2025-03-17 08:25:13 +0100

    Create ICardDeck.cs
    A Generic interface for a set of Generic Card objects
    Minimum functionality: shuffle the deck, get the top card

---
+++ BlackJackV2/Models/ICardDeck.cs
@@ -0,0 +1,21 @@
+﻿using BlackJackV2.Models.CardFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackV2.Models
+{
+
+	/* * 
+	 * A Generic interface for a set of Generic Card objects
+	 * Minimum functionality: shuffle the deck, get the top card
+	 * */
+
+	internal interface ICardDeck <TFrontImage, TBackImage, TValue>
+	{
+		public ICard<TFrontImage, TBackImage, TValue> GetTopCard();
+		public void shuffleDeck();
+	}
+}
