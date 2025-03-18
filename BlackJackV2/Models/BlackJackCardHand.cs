9d44e6f2ab8bfab7a224af888bb314f1f3faff10
a21jeaha <hisui86@gmail.com>
2025-03-18 12:47:23 +0100

    Created BlackJackCardHand and ICardHand which it inherits from
    Handels the "card hands" in the game. both player and NPC cardhands.
    this includes the calculating of the hand value, which takes into account that knights, queens, and kings all have the value of 10, and ace can have a value of either 1 or 11, depending on the current hands total value.

---
+++ BlackJackV2/Models/BlackJackCardHand.cs
@@ -0,0 +1,83 @@
+﻿using Avalonia.Media.Imaging;
+using BlackJackV2.Models.CardFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackV2.Models
+{
+	/**
+	 * Card Hand 
+	 * 
+	 * HandValue:			Get the current integer value of hand. 
+	 * Hand:				Get the list of card objects of hand
+	 * AddCard():			Adds a new card object to the hand 
+	 * ClearHand()			Emptys the hand
+	 * CalculateAceValue	Calculate the current hands value, while accounting for that ace can have a value of either 1 or 11 
+	 * 
+	 **/
+
+	internal class CardHand : ICardHand<Bitmap, Bitmap, string>
+	{
+		public List<ICard<Bitmap, Bitmap, string>> Hand {  get; private set; } 
+
+		public int HandValue
+		{ 
+			get 
+			{
+				return CalculateAceValue();
+			}
+		}
+
+		public void AddCard(ICard<Bitmap, Bitmap, string> card)
+		{
+			Hand.Add(card);
+		}
+
+		public void ClearHand()
+		{
+			Hand.Clear();
+		}
+
+		// Calculate the current hands value, while accounting for that ace can have a value of either 1 or 11 
+		public int CalculateAceValue() 
+		{
+			int currentHandValue = 0;
+			int aceCount = 0;
+			 
+			// Sums the values of the current except for aces which it counts.
+			foreach (ICard<Bitmap, Bitmap, string> card in Hand)
+			{
+				// Seperates int value and suite
+				string[] valueString = card.Value.Split('_');
+
+				// If king, Queen or Knight then value = 10, all other values (excluding ace) keep their value
+				if (int.TryParse(valueString[0], out int value) && value == 1) aceCount++;
+				else currentHandValue += value > 10 ? 10 : value;
+			}
+
+			// Ace card(s) were present in hand
+			/**
+			 *	Ace recives the value of 11 iff the total value of the hand does not exceed the value of 21. 
+			 *	When checking an ace value and there are more aces present in the hand, then 
+			 *	(currentValueOfHand + 11 + numberOfAcesLeft) cannot exceed 21, otherwise ace gets the value 1.
+			 */
+			if (aceCount > 0) 
+			{
+				while (aceCount > 0) 
+				{
+					aceCount--;
+					int handValueAttempt = (currentHandValue + 11);
+
+					if (handValueAttempt <= 21 && (handValueAttempt + aceCount) <= 21) currentHandValue += 11;
+					else currentHandValue++;
+				}
+			}
+
+			return currentHandValue;
+		}
+
+	}
+}
---
+++ BlackJackV2/Models/ICardHand.cs
@@ -0,0 +1,28 @@
+﻿using BlackJackV2.Models.CardFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackV2.Models
+{
+	/**
+	 * Card Hand interface 
+	 * Basic funtionallity present
+	 * 
+	 * HandValue:	Get the current integer value of hand. 
+	 * Hand:		Get the list of card objects of hand
+	 * AddCard():	Adds a new card object to the hand 
+	 * ClearHand()  Emptys the hand
+	 * 
+	 **/
+
+	internal interface ICardHand <TFrontImage, TBackImage, TValue>
+	{
+		public List<ICard<TFrontImage, TBackImage, TValue>> Hand { get; }
+		public int HandValue { get; }
+		public void AddCard(ICard<TFrontImage, TBackImage, TValue> card);
+		public void ClearHand();
+	}
+}
