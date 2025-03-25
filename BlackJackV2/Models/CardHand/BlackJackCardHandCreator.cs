8c1f78e41d03d20ae8ce0860190587670f26ea2a
a21jeaha <hisui86@gmail.com>
2025-03-25 12:28:38 +0100

    renamed CreatorPlayerHand to be more descriptive, also incorporated  BlackJackCardHandCreator

--- BlackJackV2/Models/CardHand/BlackJackCardHandCreator.cs
+++ BlackJackV2/Models/CardHand/BlackJackCardHandCreator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BlackJackV2.Models.CardHand
 {
-	internal class CreatorBlackJackCardHand
+	internal class BlackJackCardHandCreator
 	{
 		public BlackJackCardHand CreateBlackJackCardHand()
 		{
---
+++ BlackJackV2/Models/Player/BlackJackPlayerHandCreator.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Media.Imaging;
+using BlackJackV2.Models.CardHand;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackV2.Models.Player
+{
+	internal class BlackJackPlayerHandCreator
+	{
+		public PlayerHand CreateBlackJackPlayerHand()
+		{
+			BlackJackCardHandCreator blackJackCardHandCreator = new BlackJackCardHandCreator();
+
+			return new PlayerHand(blackJackCardHandCreator.CreateBlackJackCardHand());
+		}
+	}
+}
