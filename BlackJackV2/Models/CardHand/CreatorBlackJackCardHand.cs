7be7a2985ef4d2de25031f0fd2b3b6490c9d67d6
a21jeaha <hisui86@gmail.com>
2025-03-25 11:13:49 +0100

    added creator classes for BlackJackCardHand and PlayerHand

---
+++ BlackJackV2/Models/CardHand/CreatorBlackJackCardHand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackV2.Models.CardHand
+{
+	internal class CreatorBlackJackCardHand
+	{
+		public BlackJackCardHand CreateBlackJackCardHand()
+		{
+			return new BlackJackCardHand();
+		}
+	}
+}
---
+++ BlackJackV2/Models/Player/CreatorPlayerHand.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Media.Imaging;
+using BlackJackV2.Models.CardHand;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackV2.Models.Player
+{
+	internal class CreatorPlayerHand
+	{
+		public PlayerHand CreatePlayerHand(ICardHand<Bitmap, Bitmap, string> cardHand)
+		{
+			return new PlayerHand(cardHand);
+		}
+	}
+}
