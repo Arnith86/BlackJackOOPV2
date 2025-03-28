f141a29a4055180fae6974041ed38a06c55a876e
a21jeaha <hisui86@gmail.com>
2025-03-28 09:11:19 +0100

    the creation of viewmodels has been moved to MainWindowViewModel
    to follow convention..

--- BlackJackV2/ViewModels/MainWindowViewModel.cs
+++ BlackJackV2/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,43 @@
-﻿namespace BlackJackV2.ViewModels
+﻿using Avalonia.Media.Imaging;
+using BlackJackV2.Models;
+using BlackJackV2.Models.CardDeck;
+using BlackJackV2.Models.CardFactory;
+using BlackJackV2.Models.CardHand;
+using BlackJackV2.Models.Player;
+using ReactiveUI;
+using System.Collections.ObjectModel;
+
+namespace BlackJackV2.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+	public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
-    }
+		Bitmap _testImage;
+		private GameLogic gameLogic = new GameLogic();
+
+		public CardHandViewModel DealerCardViewModel { get; }
+		public CardHandViewModel PlayerCardViewModel { get; }
+		public ObservableCollection<CardHandViewModel> PlayerCardViewModels { get; }
+		public ButtonViewModel ButtonViewModel { get; }
+
+		public MainWindowViewModel()
+		{
+			DealerCardViewModel = ViewModelCreator.CreateHandCardViewModel(gameLogic.DealerCardHand.PrimaryCardHand.Hand);
+			PlayerCardViewModels = new ObservableCollection<CardHandViewModel>();
+			PlayerCardViewModels.Add(ViewModelCreator.CreateHandCardViewModel(gameLogic.PlayerCardHand.PrimaryCardHand.Hand));
+			ButtonViewModel = ViewModelCreator.CreateButtonViewModel(gameLogic);
+
+		}
+		public void OnPlayerSplit(string splitValue)
+		{
+			PlayerCardViewModels.Add(ViewModelCreator.CreateHandCardViewModel(gameLogic.PlayerCardHand.SplitCardHand.Hand));
+		}
+		
+
+
+		public Bitmap TestImage
+		{
+			get => _testImage;
+			set => this.RaiseAndSetIfChanged(ref _testImage, value);
+		}
+	}
 }
--- BlackJackV2/ViewModels/ViewModelBase.cs
+++ BlackJackV2/ViewModels/ViewModelBase.cs
@@ -1,8 +1,11 @@
-﻿using ReactiveUI;
+﻿using BlackJackV2.Models;
+using ReactiveUI;
+using System.Collections.ObjectModel;
 
 namespace BlackJackV2.ViewModels
 {
     public class ViewModelBase : ReactiveObject
     {
-    }
+ 
+	}
 }
