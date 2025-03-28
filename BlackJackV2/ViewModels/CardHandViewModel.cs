e3064819e770f7498f5acf127cb2bf1ff8393dba
a21jeaha <hisui86@gmail.com>
2025-03-28 09:51:12 +0100

    both player and dealer hand now show cardViews
    Only the dealer cards could be seen before, did not understand that I had to access the viewModels individually.
    I now use ItemControl to access each instance of the player cardhand viewmodel

--- BlackJackV2/ViewModels/CardHandViewModel.cs
+++ BlackJackV2/ViewModels/CardHandViewModel.cs
@@ -13,13 +13,10 @@ namespace BlackJackV2.ViewModels
 {
 	public class CardHandViewModel : ReactiveObject
 	{
-		//private List<ICard<Bitmap, Bitmap, string>> _cards;
-		//public List<ICard<Bitmap, Bitmap, string>> Cards =>  _cards;
-
 		private ObservableCollection<ICard<Bitmap, Bitmap, string>> _cards;
 		public ObservableCollection<ICard<Bitmap, Bitmap, string>> Cards => _cards;
 
-		public CardHandViewModel(ObservableCollection<ICard<Bitmap, Bitmap, string>>/*List<ICard<Bitmap, Bitmap, string>>*/ cards) 
+		public CardHandViewModel(ObservableCollection<ICard<Bitmap, Bitmap, string>> cards) 
 		{
 			_cards = cards;
 		}
