8abfeabb0e2f76351054494173faf05012d924fb
a21jeaha <hisui86@gmail.com>
2025-03-28 09:09:23 +0100

    View and ViewModel for CardHands and a ViewModel Creator have been created
    as of now only the bare minimum have been implemented.
    I will start with implementing a basic "new card" function to test the combination of button and cardhand views/viewmodels

---
+++ BlackJackV2/ViewModels/CardHandViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
+using BlackJackV2.Models;
+using BlackJackV2.Models.CardFactory;
+using ReactiveUI;
+
+namespace BlackJackV2.ViewModels
+{
+	public class CardHandViewModel : ReactiveObject
+	{
+		//private List<ICard<Bitmap, Bitmap, string>> _cards;
+		//public List<ICard<Bitmap, Bitmap, string>> Cards =>  _cards;
+
+		private ObservableCollection<ICard<Bitmap, Bitmap, string>> _cards;
+		public ObservableCollection<ICard<Bitmap, Bitmap, string>> Cards => _cards;
+
+		public CardHandViewModel(ObservableCollection<ICard<Bitmap, Bitmap, string>>/*List<ICard<Bitmap, Bitmap, string>>*/ cards) 
+		{
+			_cards = cards;
+		}
+	}
+}
---
+++ BlackJackV2/Views/CardHandView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using BlackJackV2.ViewModels;
+
+namespace BlackJackV2.Views;
+
+public partial class CardHandView : UserControl
+{
+    public CardHandView()
+    {
+        InitializeComponent();
+    }
+}
