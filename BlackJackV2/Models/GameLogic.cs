903c0fdb3d75262354aaa5f7ee67776f64b24da9
a21jeaha <hisui86@gmail.com>
2025-03-28 09:16:19 +0100

    base for testing has been implemented.
    The Model file GameLogic will act as a mediator between the viewModels and the rest of the model files.
    It creates the card deck and hands that are to be used.
    the bare minimum of what is needed to se a card and the buttons have been implemented in MainWindow.
    
    i will continue to add functionality allowing more cards to be added and new views when more hands are added.

---
+++ BlackJackV2/Models/GameLogic.cs
@@ -0,0 +1,60 @@
+﻿using Avalonia.Media.Imaging;
+using BlackJackV2.Models.CardDeck;
+using BlackJackV2.Models.CardFactory;
+using BlackJackV2.Models.CardHand;
+using BlackJackV2.Models.Player;
+using BlackJackV2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackV2.Models
+{
+	public class GameLogic
+	{
+		// Used to create a deck of cards
+		private BlackJackCardDeckCreator blackJackCardDeckCreator = new BlackJackCardDeckCreator();
+		private ICardDeck<Bitmap, Bitmap, string> blackJackCardDeck;
+		
+		// Represents the player and dealer hands
+		IPlayerHands<Bitmap, Bitmap, string> _playerCardHand;
+		IPlayerHands<Bitmap, Bitmap, string> _dealerCardHand;
+
+		public IPlayerHands<Bitmap, Bitmap, string> PlayerCardHand { get => _playerCardHand; }
+		public IPlayerHands<Bitmap, Bitmap, string> DealerCardHand { get => _dealerCardHand; }
+
+		public GameLogic()
+		{
+			blackJackCardDeck = blackJackCardDeckCreator.CreateBlackJackCardDeck();
+			_playerCardHand = BlackJackPlayerHandsCreator.CreateBlackJackPlayerHand();
+			_dealerCardHand = BlackJackPlayerHandsCreator.CreateBlackJackPlayerHand();
+
+			blackJackCardDeck.ShuffleDeck();
+			//playerCardHand.AddCard(blackJackCardDeck.GetTopCard());
+			//int temp = playerCardHand.HandValue;
+			//playerCardHand.AddCard(blackJackCardDeck.GetTopCard());
+			//temp = playerCardHand.HandValue;
+			//playerCardHand.AddCard(blackJackCardDeck.GetTopCard());
+			//temp = playerCardHand.HandValue;
+			//playerCardHand.AddCard(blackJackCardDeck.GetTopCard());
+			//temp = playerCardHand.HandValue;
+			//cardHand.AddCard(blackJackCardDeck.GetTopCard());
+			//temp = cardHand.HandValue;
+
+			//IPlayerHand<Bitmap, Bitmap, string> playerHand = new PlayerHand(playerCardHand);
+			PlayerAction playerAction = new PlayerAction();
+			playerAction.Split("Clubs_1", _playerCardHand);
+
+			// TestImage = cardHand.Hand[0].FrontImage;
+		}
+
+		public void AddCard(/*ICardHand<Bitmap, Bitmap, string> cardHand, BlackJackCardDeck blackJackCardDeck*/)
+		{
+			Console.WriteLine("KAKAKA");
+			//cardHand.AddCard(blackJackCardDeck.GetTopCard());
+		}
+	}
+}
