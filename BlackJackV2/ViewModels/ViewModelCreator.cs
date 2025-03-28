de34b70875f63f86364f8b6cbb3af6c6e542c652
a21jeaha <hisui86@gmail.com>
2025-03-28 09:10:00 +0100

    Create ViewModelCreator.cs
    forgot to ad the viewmodel creator in the last commit, so here it is

---
+++ BlackJackV2/ViewModels/ViewModelCreator.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Media.Imaging;
+using BlackJackV2.Models;
+using BlackJackV2.Models.CardHand;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackV2.ViewModels
+{
+	public class ViewModelCreator
+	{
+		public static CardHandViewModel CreateHandCardViewModel(/*List<ICard<Bitmap, Bitmap, string>>*/  ObservableCollection<ICard<Bitmap, Bitmap, string>> cardHand)
+		{
+			return new CardHandViewModel(cardHand);
+		}
+		public static ButtonViewModel CreateButtonViewModel(GameLogic gameLogic)
+		{
+			return new ButtonViewModel(gameLogic);
+		}
+	}
+}
