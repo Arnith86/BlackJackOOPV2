acb1072606d9fe0a9b041cb97be797397dffb604
a21jeaha <hisui86@gmail.com>
2025-04-22 14:05:56 +0200

    Update BlackJackCard.cs
    removed old summary

--- BlackJackV2/Models/Card/BlackJackCard.cs
+++ BlackJackV2/Models/Card/BlackJackCard.cs
@@ -1,23 +1,6 @@
 ﻿// Project: BlackJackV2
 // file: BlackJackV2/Models/CardFactory/BlackJackCard.cs
 
-/// <summary>
-///		
-///		A single card used in black jack. 
-/// 
-///		Bitmap		CurrentCardImage				:	The current image that shown (front or back of card).
-///		Bitmap		FrontImage						:	The visual representation of the card value.
-///		Bitmap		BackImage						:	The visual representation of the back of the card value 
-///		string[]	Value							:   Has one of four colors ( Heart, Dimond, Spade, Club ), 
-///															and a numerical value between 1 - 13 (knight: 11, queen: 12, king: 13)
-///		bool		FaceDown						:	Signifies if the back of the card is shown or the value
-///		readonly	CompositeDisposable _disposable
-///		
-///		void		flipCard()						:	Flips the card to show the opposite side	
-///		void 		dispose()						:	Clean up resources
-///		
-/// </summary>
-
 using Avalonia.Media.Imaging;
 using ReactiveUI;
 using System;
