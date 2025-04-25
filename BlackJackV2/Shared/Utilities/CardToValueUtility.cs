ee6d0d8e765011680292c130f148714ab7212522
a21jeaha <hisui86@gmail.com>
2025-04-25 08:50:47 +0200

    Added summary to CardToValueUtility.cs

--- BlackJackV2/Shared/Utilities/CardToValueUtility.cs
+++ BlackJackV2/Shared/Utilities/CardToValueUtility.cs
@@ -7,9 +7,23 @@ using System.Linq;
 
 namespace BlackJackV2.Shared.UtilityClasses
 {
+	/// <summary>
+	/// This utility class is used for extracting a card numeric and suit representation.
+	/// </summary>
 	public static class CardToValueUtility
 	{
+		/// <summary>
+		/// Extracts the numerical value of the card.
+		/// </summary>
+		/// <param name="card">The card from which to extract the value.</param>
+		/// <returns></returns>
 		public static string GetNumericCardValue(ICard<Bitmap, string> card) => card.Value.Split('_').Last();
+		
+		/// <summary>
+		/// Extracts the suite value of the card.
+		/// </summary>
+		/// <param name="card">The card from which to extract the suite.</param>
+		/// <returns></returns>
 		public static string GetCardSuit(ICard<Bitmap, string> card) => card.Value.Split('_').First();
 	}
 }
