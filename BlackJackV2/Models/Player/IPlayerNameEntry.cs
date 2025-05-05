cb23503bf9f0013635d946b299770ec80cf77735
a21jeaha <hisui86@gmail.com>
2025-05-05 09:44:10 +0200

    Created I & PlayerNameEntry used for player names

---
+++ BlackJackV2/Models/Player/IPlayerNameEntry.cs
@@ -0,0 +1,21 @@
+﻿// Project: BlackJackV2
+// File: Models/Player/IPlayerNameEntry.cs
+
+namespace BlackJackV2.Interfaces
+{
+	/// <summary>
+	/// Represents a contract for accessing and modifying a player's name entry.
+	/// </summary>
+	public interface IPlayerNameEntry
+	{
+		/// <summary>
+		/// Gets the unique index of the player.
+		/// </summary>
+		int PlayerIndex { get; }
+
+		/// <summary>
+		/// Gets or sets the player's name.
+		/// </summary>
+		string PlayerName { get; set; }
+	}
+}
--- BlackJackV2/Models/Player/PlayerNameEntry.cs
+++ BlackJackV2/Models/Player/PlayerNameEntry.cs
@@ -1,22 +1,37 @@
 ﻿// Project: BlackJackV2
 // File: Models/Player/PlayerNameEntry.cs
 
+using BlackJackV2.Interfaces;
 using ReactiveUI;
 
 namespace BlackJackV2.Models.Player
 {
-	public class PlayerNameEntry : ReactiveObject
+	/// <summary>
+	/// Represents a player's name entry with a unique index and a name that supports data binding.
+	/// </summary>
+	public class PlayerNameEntry : ReactiveObject, IPlayerNameEntry
 	{
+		/// <summary>
+		/// Gets the index of the player (used to identify and order players).
+		/// </summary>
 		public int PlayerIndex { get; }
 
 		private string _playerName;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PlayerNameEntry"/> class.
+		/// </summary>
+		/// <param name="index">The unique index of the player.</param>
+		/// <param name="name">The player's display name.</param>
 		public PlayerNameEntry(int index, string name)
 		{
 			PlayerIndex = index;
 			_playerName = name; 
 		}
 
+		/// <summary>
+		/// Gets or sets the player's name. Notifies the UI when changed.
+		/// </summary>
 		public string PlayerName
 		{
 			get => _playerName;
